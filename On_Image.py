44e78fec5e9302a94dab64f8013d467c8d41abe9
Mohammad Parsa Rezaifar <144287094+Parsa-Rezaifar@users.noreply.github.com>
2024-07-17 16:42:17 +0330

    Update On_Image.py

--- On_Image.py
+++ On_Image.py
@@ -2,9 +2,14 @@
 from ultralytics import YOLO
 import cv2
 
-# Introduce your pytorch model(.pt) to YOLO
+"""
+In this code you use the detection model on images
+Enter your image path with its format in single or double quotation
+Example = "my_image_path.jpg"
+"""
+
+# Introduce your pytorch model(.pt) to YOLO , in single or double quotation
 model = YOLO()
-# Your image path
 results = model(source="",show=True)
 cv2.waitKey()
 cv2.destroyAllWindows()
